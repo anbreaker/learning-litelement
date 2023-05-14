24cc5e1016677b7f2d2ef7a168419d3c30737f31
anbreaker <antunez19@gmail.com>
2023-05-14 11:15:35 +0200

    feat: build

--- litelement-rick-morty-api/web-dev-server.config.js
+++ litelement-rick-morty-api/web-dev-server.config.js
@@ -10,9 +10,9 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   nodeResolve: {
     exportConditions: ['browser', 'development'],
   },
-  
+
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
-  // esbuildTarget: 'auto'
+  esbuildTarget: ['auto'],
 
   /** Set appIndex to enable SPA routing */
   appIndex: './index.html',
